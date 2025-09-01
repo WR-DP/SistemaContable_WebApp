cea92782a4a090f5d89506a610a1c992502f7e6f
sauron <dp20007@ues.edu.sv>
2025-09-01 12:20:32 -0600

    estructura inicial deproyecto

---
+++ src/main/java/sv/edu/ues/occ/ingenieria/sic135/registro/web/registrocontable/SistemaRegistroContableWebAppApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.occ.ingenieria.sic135.registro.web.registrocontable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRegistroContableWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaRegistroContableWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sv/edu/ues/occ/ingenieria/sic135/registro/web/registrocontable/SistemaRegistroContableWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.occ.ingenieria.sic135.registro.web.registrocontable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRegistroContableWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
